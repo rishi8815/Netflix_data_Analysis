fb3048f9910f80f322087979c2136542ea7e9bde
rishi8815 <147094465+rishi8815@users.noreply.github.com>
2025-02-14 00:57:17 +0530

    Update Problem_Netflix.sql

--- Problem_Netflix.sql
+++ Problem_Netflix.sql
@@ -1,21 +1,19 @@
 -- 15 Business Problems & Solutions
 
-1. Count the number of Movies vs TV Shows
-2. Find the most common rating for movies and TV shows
-3. List all movies released in a specific year (e.g., 2020)
-4. Find the top 5 countries with the most content on Netflix
-5. Identify the longest movie
-6. Find content added in the last 5 years
-7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
-8. List all TV shows with more than 5 seasons
-9. Count the number of content items in each genre
-10.Find each year and the average numbers of content release in India on netflix. 
-return top 5 year with highest avg content release!
-11. List all movies that are documentaries
-12. Find all content without a director
-13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
-14. Find the top 10 actors who have appeared in the highest number of movies produced in India.
-15.
+1.Count the Number of Movies vs TV Shows
+2.Find the Earliest and Latest Release Year List All Movies Released in a Specific Year (e.g., 2020)
+3.Find the Most Common Rating on Netflix
+4.List the Top 5 Most Common Genres
+5.Find the Total Number of TV Shows Released After 2015
+6.Get a List of Unique Countries in the Dataset
+7.Count the Number of Movies and Shows per Year
+8.Retrieve All Content Where Cast Includes 'Leonardo DiCaprio'
+9. All Movies Released Between 2010 and 2020
+10.List All Movies that are Documentaries
+11.Find the Top 5 Directors with the Most Movies on Netflix
+12. Find How Many Movies Actor 'Salman Khan' Appeared in the Last 10 Years
+13.Find All Content That Does Not Have a Director Listed
+14.Find the Number of Movies Available in Each Country
 Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
 the description field. Label content containing these keywords as 'Bad' and all other 
 content as 'Good'. Count how many items fall into each category.
